61e392a168ff0b6277f61dfe46ae28f49185a0c7
ILHAM <IlhamMuzani>
2023-06-05 12:47:24 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toogle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+//ketika dokumen menu di clik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+//klik di luar sidebar untuk menghilangkan menu atau nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
